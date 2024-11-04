5e8a4a1ae4d4b8594405e62c1f1e5233e9f239be
Chris Garrett <chris@nesteggs.ca>
2024-11-04 11:31:23 -0700

    copy config from other repo, use home screen as initial view

--- apps/remix/app/routes/_index.tsx
+++ apps/remix/app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from '@remix-run/node'
 import { styled, Text, View } from '@my/ui'
 import { useState } from 'react'
+import { HomeScreen } from 'app/features/home/screen'
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,45 +12,6 @@ export const meta: MetaFunction = () => {
     },
   ]
 }
-
-const Section = styled(View, {
-  tag: 'section',
-  padding: '$large',
-  gap: '$large',
-  $smallScreen: {
-    padding: '$medium',
-  },
-  $mediumScreen: {
-    padding: '$large',
-    maxWidth: 600,
-  },
-  $largeScreen: {
-    padding: '$extraLarge',
-  },
-})
-
-const ButtonOuter = styled(View, {
-  tag: 'button',
-  themeInverse: true,
-  backgroundColor: '$background',
-  borderRadius: '$medium',
-  cursor: 'pointer',
-  padding: '$medium',
-  maxWidth: 'fit-content',
-  display: 'unset',
-  pressStyle: {
-    opacity: 0.8,
-  },
-  hoverStyle: {
-    opacity: 0.9,
-  },
-})
-
-const ButtonText = styled(Text, {
-  fontWeight: 'bold',
-  textAlign: 'center',
-  tag: 'span',
-})
 
 export default function Index() {
   const [theme, setTheme] = useState('light')
@@ -64,94 +26,7 @@ export default function Index() {
       backgroundColor="$background"
       minHeight="100vh"
     >
-      <View tag="header" padding="$large" borderBottomWidth={1} borderBottomColor="$borderColor">
-        <Text
-          tag="h1"
-          fontSize={24}
-          $largeScreen={{
-            fontSize: 32,
-          }}
-        >
-          Welcome to Tamagui with Remix
-        </Text>
-      </View>
-      <Section>
-        <Text
-          tag="h2"
-          fontSize={24}
-          $largeScreen={{
-            fontSize: 32,
-          }}
-        >
-          Introduction
-        </Text>
-        <Text>This is a demo page to showcase how Tamagui works seamlessly with Remix.</Text>
-      </Section>
-      <Section>
-        <Text
-          tag="h2"
-          fontSize={24}
-          $largeScreen={{
-            fontSize: 32,
-          }}
-        >
-          Features
-        </Text>
-        <Text>
-          Tamagui provides a powerful and flexible way to style your React components. With Tamagui,
-          you can leverage themes to create a consistent look and feel across your application.
-          Theming in Tamagui is highly customizable, allowing you to define colors, spacing,
-          typography, and more. You can easily switch between light and dark themes, or create your
-          own custom themes to match your brand.
-        </Text>
-      </Section>
-      <Section>
-        <Text
-          tag="h2"
-          fontSize={24}
-          $largeScreen={{
-            fontSize: 32,
-          }}
-        >
-          Works with themes
-        </Text>
-        <Text>
-          Tamagui&apos;s styling system is designed to work with Remix out of the box. Server-render
-          initial styles and themes.
-        </Text>
-        <ButtonOuter onPress={toggleTheme}>
-          <ButtonText>Toggle Theme</ButtonText>
-        </ButtonOuter>
-      </Section>
-      <Section>
-        <Text
-          tag="h2"
-          fontSize={24}
-          $largeScreen={{
-            fontSize: 32,
-          }}
-        >
-          Go Native
-        </Text>
-        <Text>
-          One of the standout features of Tamagui is its ability to seamlessly port your components
-          to React Native. This means you can write your components once and run them on both web
-          and mobile platforms. Tamagui&apos;s styling system is designed to work with React Native
-          out of the box, so you can take advantage of native performance and capabilities without
-          having to rewrite your components.
-        </Text>
-      </Section>
-      <View tag="footer" padding="$large" justifyContent="center" marginTop="auto" paddingTop={100}>
-        <Text
-          tag="p"
-          fontSize={16}
-          $largeScreen={{
-            fontSize: '$3',
-          }}
-        >
-          © {new Date().getFullYear()} Tamagui with Remix.
-        </Text>
-      </View>
+      <HomeScreen />
     </View>
   )
 }
--- apps/remix/vite.config.ts
+++ apps/remix/vite.config.ts
@@ -3,7 +3,39 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { tamaguiPlugin } from '@tamagui/vite-plugin'
 
+const extensions = [
+  '.web.js',
+  '.web.jsx',
+  '.web.ts',
+  '.web.tsx',
+  '.mjs',
+  '.js',
+  '.mts',
+  '.ts',
+  '.jsx',
+  '.tsx',
+  '.json',
+]
+
 export default defineConfig({
+  define: {
+    DEV: `${process.env.NODE_ENV === 'development' ? true : false}`,
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  },
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web',
+    },
+    extensions: extensions,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      resolveExtensions: extensions,
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
   plugins: [
     remix({
       future: {
@@ -15,8 +47,9 @@ export default defineConfig({
     tsconfigPaths(),
     tamaguiPlugin({
       config: '../../packages/config/src/tamagui.config.ts',
-      optimize: true,
+      optimize: false,
       outputCSS: './app/tamagui.css',
+      components: ['tamagui'],
     }),
-  ],
+  ].filter(Boolean),
 })
